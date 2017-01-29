27031c760aa3929acaeceb5f96a7bf288fecacf5
Callum Browne <me+github@calbro.me>
2017-01-29 23:00:45 +0000

    Added initial class definitions

--- src/Node.php
+++ src/Node.php
@@ -1,3 +1,20 @@
 <?php
 
 namespace cal7\trie\Node;
+
+class Node{
+    private $character;
+    private $isEndOfWord;
+    private $children = [];
+
+    public function __construct($character, $isEndOfWord)
+    {
+        $this->character = $character;
+        $this->isEndOfWord = $isEndOfWord;
+    }
+
+    public function addChild($node)
+    {
+        $this->children[$node->character] = $node;
+    }
+}
--- src/Trie.php
+++ src/Trie.php
@@ -1,3 +1,14 @@
 <?php
 
 namespace cal7\trie\Trie;
+
+use cal7\trie\Node;
+
+class Trie{
+    private $root;
+
+    public function __construct()
+    {
+        $this->root = new Node("", false);
+    }
+}
